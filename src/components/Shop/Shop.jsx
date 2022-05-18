fe642b801430ae9d2159198bedb4ef7e114921a7
Danney Trieu <89677528+Danneytrieu@users.noreply.github.com>
2022-05-18 16:01:48 -0600

    updated card

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -14,34 +14,50 @@ const Container = styled.section`
   justify-content: center;
   padding: 30px;
   color: var(--color-green);
-  gap: 20px;
   background: var(--color-vanilla);
+  gap: 20px;
+  /* Heading */
+  h2{
+
+  }
+  
+  /* Subheading */
+  h3{
+
+  }
+  /* buttons */
+  section{
+
+  }
 `;
-const Products = styled.div`
+const ProductContainer = styled.div`
   display: flex;
+  gap: 70px;
 `;
+// Individual card
 const ProductCard = styled.div`
-width: 15vw;
+  width: 15vw;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 8px;
   img {
-    border-radius: 10px;
+    border-radius: 40px;
     width: 100%;
   }
-  h6{
+  h6 {
     font-size: 1.3rem;
     font-weight: 400;
   }
-  h7{
-    font-weight:200;
-  };
-  span{
+  h7 {
+    font-weight: 200;
+  }
+  span {
     font-size: 1.2rem;
   }
 `;
+// Icon
 const Star = styled(AiFillStar)`
   color: var(--color-gold);
   font-size: 1.5rem;
@@ -58,7 +74,7 @@ export const Shop = () => {
         <button>CANNABIS</button>
         <button>EDIBLES</button>
       </section>
-      <Products>
+      <ProductContainer>
         <ProductCard>
           <img src={shop1} alt="" />
           <div>
@@ -74,7 +90,52 @@ export const Shop = () => {
           </h7>
           <span>$13.99</span>
         </ProductCard>
-      </Products>
+        <ProductCard>
+          <img src={shop2} alt="" />
+          <div>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+          </div>
+          <h6>Danney Haze</h6>
+          <h7>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          </h7>
+          <span>$13.99</span>
+        </ProductCard>
+        <ProductCard>
+          <img src={shop3} alt="" />
+          <div>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+          </div>
+          <h6>Danney Haze</h6>
+          <h7>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          </h7>
+          <span>$13.99</span>
+        </ProductCard>
+        <ProductCard>
+          <img src={shop4} alt="" />
+          <div>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+          </div>
+          <h6>Danney Haze</h6>
+          <h7>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          </h7>
+          <span>$13.99</span>
+        </ProductCard>
+      </ProductContainer>
     </Container>
   );
 };
