b1b635ed41f9bf75e1e7ff01d1f73121fe4f1e16
Danney Trieu <89677528+Danneytrieu@users.noreply.github.com>
2022-05-18 18:29:19 -0600

    test

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,19 +1,55 @@
 import React from "react";
 import alienImage from "../../assets/images/alien.png";
 import mapImage from "../../assets/images/map.png";
+import styled from "styled-components";
+import { device } from "../../device";
+
+const Container = styled.section`
+  width: 100%;
+  display: flex;
+  text-align: center;
+  padding: 100px;
+  @media ${device.laptop} {
+    flex-direction: column;
+    align-items: center;
+    padding: 50px;
+  }
+  img {
+    width: 100%;
+    height: fit-content;
+  }
+  h2 {
+    font-family: var(--font-halant);
+    font-size: 3rem;
+    font-weight: 200;
+    color: var(--color-gold);
+  }
+  h3 {
+    font-weight: 100;
+  }
+  div {
+    width: 50%;
+    @media ${device.laptop}{
+      width: 100%;
+    }
+  }
+`;
 
 export const Map = () => {
   return (
-    <section>
+    <Container>
       <div>
         <h2>Visit The Coffee Joint</h2>
         <h3>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-          nesciunt eos non distinctio esse quod, animi nisi accusamus.
+          nesciunt eos non distinctio esse quod, animi nisi accusamus. Nesciunt
+          eos non distinctio esse quod, animi nisi accusamus.
         </h3>
         <img src={mapImage} alt="" />
       </div>
-      <img src={alienImage} alt="" />
-    </section>
+      <div>
+        <img src={alienImage} alt="" />
+      </div>
+    </Container>
   );
 };
