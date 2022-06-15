bdf733d63296d4fc99760a81f4c4dd38038f61c3
Danney Trieu <89677528+Danneytrieu@users.noreply.github.com>
2022-06-15 15:55:25 -0600

    update footer reference link

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+
 import { ParallaxProvider } from "react-scroll-parallax";
 import { AboutUs } from "./components/AboutUs/AboutUs";
 import { Header } from "./components/Header/Header";
 import { Grid } from "./components/Grid/Grid";
-// import { Shop } from "./components/Shop/Shop";
 import { BasicTabs } from "./components/Shop/Shop";
 import { Map } from "./components/Map/Map";
 import { Gallery } from "./components/Gallery/Gallery";
@@ -10,13 +10,14 @@ import { Footer } from "./components/Footer/Footer";
 import { FAQ } from "./components/FAQ/FAQ";
 import { ContactUs } from "./components/ContactUs/ContactUs";
 
+
 const App = () => {
+
   return (
     <ParallaxProvider>
       <Header />
       <AboutUs />
       <Grid />
-      {/* <Shop /> */}
       <BasicTabs />
       <ContactUs />
       <FAQ />
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import logoImage from "../../assets/images/logo.png";
 import * as S from "./Footer.styled";
 
@@ -10,37 +11,107 @@ export const Footer = () => {
         <div>
           <img src={logoImage} alt="" />
           <h2>
-            Medical cannabis can be administered through various methods,
-            including capsules, lozenges, tinctures, dermal patches.
+            As part of Yuma Way Cannabis, the staff at The Coffee Joint share a
+            passion to de-stigmatize cannabis consumption and are available to
+            answer any questions!
           </h2>
           <article>
-            <S.InstagramIcon /> <S.FacebookIcon /> <S.YoutubeIcon />{" "}
-            <S.GoogleIcon />
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.instagram.com/the_coffee_joint_/"
+            >
+              <S.InstagramIcon />
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.facebook.com/TheCoffeeJointDenver/"
+            >
+              <S.FacebookIcon />
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.youtube.com/channel/UCrOT5AOpeOEXMyh6j_EJH_g"
+            >
+              <S.YoutubeIcon />
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://goo.gl/maps/vWWNrCpmixsmeXBW9"
+            >
+              <S.GoogleIcon />
+            </a>
           </article>
         </div>
+
         {/* column 2 */}
         <div className="hideForTablet">
           <span>About Us</span>
           <a href="/">Contact Us</a>
-          <a href="/">Privacy Policy</a>
+          <a href="/">Direction</a>
           <a href="/">Terms of Service</a>
-          <a href="/">Carrers</a>
         </div>
+
         {/* column 3 */}
         <div className="hideForTablet">
           <span>Products</span>
-          <a href="/">Shop</a>
-          <a href="/">Edibles</a>
-          <a href="/">Oil</a>
-          <a href="/">Buds</a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://yumaway.com/locations/menu/17?"
+          >
+            Shop
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://yumaway.com/locations/menu/17?"
+          >
+            Edibles
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://yumaway.com/locations/menu/17?"
+          >
+            Oil
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://yumaway.com/locations/menu/17?"
+          >
+            Buds
+          </a>
         </div>
+
         {/* column 4 */}
-        <div >
+        <div>
           <span>Contact Info</span>
-          <h5>1130 Yuma Court, Denver, CO 80204</h5>
-          <h5>(720) 583-4657</h5>
-          <h5>thecoffeejointco.com</h5>
-          <h5>thecoffeejoint@yumaway.com</h5>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.google.com/maps/dir//the+coffee+joint/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x876c7f4b51569687:0xb8e67a05179bf2c4?sa=X&ved=2ahUKEwjo1fX05Z74AhVGSzABHUebCy0Q9Rd6BAhLEAU"
+          >
+            <h5>1130 Yuma Court, Denver, CO 80204</h5>
+          </a>
+          <a href="tel:(720) 583-4657">
+            <h5>(720) 583-4657</h5>
+          </a>
+
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.thecoffeejointco.com"
+          >
+            <h5>thecoffeejointco.com</h5>
+          </a>
+          <a href="mailto:thecoffeejoint@yumaway.com">
+            <h5>thecoffeejoint@yumaway.com</h5>
+          </a>
         </div>
       </S.FooterContainer>
       <S.CopyRight>
